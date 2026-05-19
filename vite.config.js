3ff647f2ba762a92e58d0706df75d895586b9c44
kevin98831 <125783212+kevin98831@users.noreply.github.com>
2026-05-19 17:10:20 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    legacy({
+      targets: ['Android >= 8', 'Chrome >= 61', 'iOS >= 12'],
+      modernPolyfills: true,
+    }),
+  ],
+  build: {
+    target: 'es2015',
+    cssTarget: 'chrome61',
+  },
 })
